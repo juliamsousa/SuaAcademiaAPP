d60b9a152deea31a3e6d2ea0d2c3d479055d0ba6
Júlia Miranda <juliaeduardasousa@gmail.com>
2024-02-02 11:27:05 -0300

    Adicionados assets da aplicação como: fontes, logos e arquivos SVG no formato JSX.

---
+++ assets/ArrowRight.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Svg, Path} from "react-native-svg";
+
+function ArrowRight() {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="8"
+      height="13"
+      fill="none"
+      viewBox="0 0 8 13"
+    >
+      <Path
+        fill="#fff"
+        d="M.578 11.36l4.594-4.594L.578 2.172 1.984.766l6 6-6 6-1.406-1.407z"
+      ></Path>
+    </Svg>
+  );
+}
+
+export default ArrowRight;
